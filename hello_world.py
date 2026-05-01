60921e7f67e84e935ebb02a5760ad09329d140b7
lake.dev <santiago.lago4@gmail.com>
2026-05-01 16:29:25 -0300

    python_work

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print("Hello Python wold!")
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+mesage = "Hello Python Crash Course reader!"
+print(mesage)
+
---
+++ simple_message.py
@@ -0,0 +1,8 @@
+message = ("Hello there!")
+print (message)
+
+cuerdas = ('You can use "apostrophes" whitin your strings')
+cuerdas_2 = ("Invert double or 'simple'")
+
+print(cuerdas, cuerdas_2)
+
