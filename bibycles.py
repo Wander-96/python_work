31f96d61b3a36a11c30ff3e111b7a6ade6c9e7ef
Wander-96 <santiago.lago4@gmail.com>
2026-05-06 10:30:16 -0300

    Add basic arithmetic operations and bicycle list example

---
+++ bibycles.py
@@ -0,0 +1,13 @@
+bicycles = ["trek", "cannondale", "redline", "specialized"]
+print (bicycles)
+#this isn’t the output you want your users to see, let’s learn how 
+#to access the individual items in a list. 
+print(bicycles[1])
+
+print(bicycles[2].title())
+
+print(bicycles[-1])
+
+print(bicycles[-3])
+
+#page 39
--- numbers.py
+++ numbers.py
@@ -1,1 +1,10 @@
 # page 30
+
+print(4+4)
+print(16/2)
+print(12-4)
+print(2*4)
+
+favourite_number = 18
+
+print("Cuando cumplas " + str(favourite_number) + " podras sacar tu licencia")
